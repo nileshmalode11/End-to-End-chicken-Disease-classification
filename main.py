837e08c5af7c6fbf54b1983490fee83bfefa9028
Nilesh Malode <nileshmalode9829@gmail.com>
2023-10-13 11:14:07 +0530

    modular code upadated

---
+++ main.py
@@ -0,0 +1,18 @@
+
+from src.Diesease_classifier import logger
+from src.Diesease_classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+#from src.Diesease_classifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+#from src.Diesease_classifier.pipeline.stage_03_training import ModelTrainingPipeline
+#from src.Diesease_classifier.pipeline.stage_04_evaluation import EvaluationPipeline
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
