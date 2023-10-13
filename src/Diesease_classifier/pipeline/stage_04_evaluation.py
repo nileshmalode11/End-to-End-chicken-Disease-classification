904ff9965f8f68afc04c47fc1adb6cf3f7030b84
Nilesh Malode <nileshmalode9829@gmail.com>
2023-10-13 15:32:08 +0530

    user app added

---
+++ src/Diesease_classifier/pipeline/stage_04_evaluation.py
@@ -0,0 +1,34 @@
+from src.Diesease_classifier.config.configuration import ConfigurationManager
+from src.Diesease_classifier.components.evaluation import Evaluation
+from src.Diesease_classifier import logger
+
+
+
+
+STAGE_NAME = "Evaluation stage"
+
+
+class EvaluationPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        val_config = config.get_validation_config()
+        evaluation = Evaluation(val_config)
+        evaluation.evaluation()
+        evaluation.save_score()
+
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f"*******************")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = EvaluationPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+            
